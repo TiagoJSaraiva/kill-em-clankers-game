f678b019b863c0b2838a400c6ce9889737ac69a9
TiagoJSaraiva <saraivajtiago@gmail.com>
2026-05-08 21:18:20 -0300

    Remoção do template desnecessário

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,24 +1,20 @@
-import { Boot } from './scenes/Boot';
-import { GameOver } from './scenes/GameOver';
+// import { GameOver } from './scenes/GameOver';
 import { Game as MainGame } from './scenes/Game';
-import { MainMenu } from './scenes/MainMenu';
+// import { MainMenu } from './scenes/MainMenu';
 import { AUTO, Game } from 'phaser';
-import { Preloader } from './scenes/Preloader';
 
 //  Find out more information about the Game Config at:
 //  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
 const config: Phaser.Types.Core.GameConfig = {
     type: AUTO,
-    width: 1024,
-    height: 768,
+    width: 1280,
+    height: 720,
     parent: 'game-container',
-    backgroundColor: '#028af8',
+    backgroundColor: '#20313e',
     scene: [
-        Boot,
-        Preloader,
-        MainMenu,
+        // MainMenu,
         MainGame,
-        GameOver
+        // GameOver
     ]
 };
 
