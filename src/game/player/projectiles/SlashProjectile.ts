414084dc494c019e05ffcedf842a05256010e2ac
TiagoJSaraiva <saraivajtiago@gmail.com>
2026-05-10 04:26:37 -0300

    Lógica do SlashProjectile melhor desenvolvida

--- src/game/player/projectiles/SlashProjectile.ts
+++ src/game/player/projectiles/SlashProjectile.ts
@@ -2,8 +2,42 @@ import Projectile from "./Projectile";
 
 export default class SlashProjectile extends Projectile
 {
-    constructor (scene: Phaser.Scene, x: number, y: number)
+    private updateListener: Function;
+    private lifespam: number = 20; // Tempo de vida do projétil em frames, usado para destruir o projétil após um certo tempo
+    private age: number = 0; // Idade atual do projétil em frames, incrementada a cada update
+
+    constructor (scene: Phaser.Scene, x: number, y: number, texture: string)
     {
-        super(scene, x, y, 'slash-projectile');
+        super(scene, x, y, texture);
+        this.setVelocityX(1000); // Define a velocidade do projétil para cima
+        this.updateListener = () => this.update();
+        this.scene.events.on('update', this.updateListener);
+    }
+
+    update(): void
+    {
+        // Destruir se sair dos limites da tela
+        if (this.y < -50 || this.y > 770 || this.x < -50 || this.x > 1350)
+        {
+            console.log("destroyed!");
+            this.scene.events.off('update', this.updateListener);
+            this.destroy();
+        } else {
+            this.processLifespam()
+        }
+    }
+
+    processLifespam(): void
+    {
+        /** 
+         *  Esse método é chamado em toda chamada de update() do projétil e
+         *  é responsável por controlar o tempo de vida do projétil, destruindo-o após um certo tempo.
+         */
+        this.age++;
+        if (this.age >= this.lifespam)
+        {
+            this.scene.events.off('update', this.updateListener);
+            this.destroy();
+        }
     }
 }
--- src/game/player/weapons/Weapons.ts
+++ src/game/player/weapons/Weapons.ts
@@ -1,6 +1,6 @@
 const Weapons = [
     { name: 'Pistol', attackCooldown: 10 },
-    { name: 'Sword', attackCooldown: 15 },
+    { name: 'Sword', attackCooldown: 70 },
     { name: 'Chakram', attackCooldown: 20 },
     { name: 'Launcher', attackCooldown: 25 }
 ]
