5fc9de69e2fefc45ded600a933b5ad8e9a18e21a
TiagoJSaraiva <saraivajtiago@gmail.com>
2026-06-17 03:20:26 -0300

    Correções finais

--- src/game/scenes/main-menu/MainMenu.ts
+++ src/game/scenes/main-menu/MainMenu.ts
@@ -36,7 +36,6 @@ export class MainMenu extends Scene
     constructor ()
     {
         super('MainMenu');
-        this.init();
     }
 
     preload() {
@@ -56,6 +55,111 @@ export class MainMenu extends Scene
         this.playButton = undefined;
         this.configButton = undefined;
         this.exitButton = undefined;
+    }
+
+    create ()
+    {
+        const { width, height } = this.scale;
+
+        this.backgroundCover = this.add.image(this.width / 2, this.height / 2, 'menu-bg').setDepth(-1);
+
+        this.backgroundFar = this.add.tileSprite(0, 0, width, height, 'bg-far');
+        this.backgroundFar.setOrigin(0, 0);
+        this.backgroundFar.setDepth(-20);
+
+        this.backgroundNear = this.add.tileSprite(0, 0, width, height, 'bg-near');
+        this.backgroundNear.setOrigin(0,-0.3);
+        this.backgroundNear.setDepth(-10);
+
+        this.backgroundMiddle = this.add.tileSprite(0, 0, width, height, 'bg-middle');
+        this.backgroundMiddle.setOrigin(0,-0.18);
+        this.backgroundMiddle.setDepth(-15);
+
+        this.backgroundVeryFar = this.add.tileSprite(0, 0, width, height, 'bg-very-far');
+        this.backgroundVeryFar.setOrigin(0, 0);
+        this.backgroundVeryFar.setDepth(-25);
+    }
+
+    update(time: number, delta: number): void{
+        const dt = delta / 1000;
+
+        this.backgroundFar.tilePositionX += MainMenu.PARALLAX_FAR_SPEED * 60 * dt;
+        this.backgroundNear.tilePositionX += MainMenu.PARALLAX_NEAR_SPEED * 60 * dt;
+        this.backgroundMiddle.tilePositionX += MainMenu.PARALLAX_MIDDLE_SPEED * 60 * dt;
+        this.backgroundVeryFar.tilePositionX += MainMenu.PARALLAX_VERY_FAR_SPEED * 60 * dt;
+
+        if(!this.updateChange) {
+            if(time > 2000 && !this.title) {
+                this.title = this.add.image(this.width / 2, 220, 'main-menu-title').setDepth(1);
+            }
+            if(time > 3000 && !this.subtitle) {
+                this.subtitle = this.add.image(this.width / 2, 450, 'main-menu-subtitle').setDepth(2).setScale(0.3);
+            }
+            if(time > 5000 && !this.playButton) {
+                this.playButton = this.add.image(this.width / 2, 800, 'play-button').setScale(this.buttonScale);
+            }
+            if(time > 5200 && !this.configButton) {
+                this.configButton = this.add.image(this.width / 2 - 500, 800, 'config-button').setScale(this.buttonScale);
+            }
+            if(time > 5400 && !this.exitButton) {
+                this.exitButton = this.add.image(this.width / 2 + 500, 800, 'exit-button').setScale(this.buttonScale);
+            }
+            if(this.playButton && this.configButton && this.exitButton && !this.buttonsActive) {
+                this.setupButtons();
+            }
+            if(time > 6000 && !this.bgTween && !this.fadeOutStarted) {
+                this.fadeOutStarted = true;
+
+                this.bgTween = this.tweens.add({
+                    targets: this.backgroundCover,
+                    alpha: 0,
+                    duration: 500,
+                    ease: 'Linear',
+                });  
+            }
+
+        } else {
+            let _time = time - this.timeAtUpdateChange;
+
+            if(this.playButton) {
+                this.playButton.destroy();
+                this.playButton = undefined;
+            }
+
+            this.buttonsActive = false;
+
+            if(_time > 2000 && this.title) {
+                this.title.destroy();
+                this.title = undefined;
+            }
+            if(_time > 1200 && this.subtitle) {
+                this.subtitle.destroy();
+                this.subtitle = undefined;    
+            }
+            if(_time > 200 && this.configButton) {
+                this.configButton.destroy();
+                this.configButton = undefined;
+            }
+            if(_time > 400 && this.exitButton) {
+                this.exitButton.destroy();
+                this.exitButton = undefined;
+            }
+            if(_time > 3000 && !this.fadeInStarted) {
+                this.fadeInStarted = true;
+
+                this.bgTween?.stop();
+
+                this.bgTween = this.tweens.add({
+                    targets: this.backgroundCover,
+                    alpha: 1,
+                    duration: 500,
+                    ease: 'Linear',
+                });
+            }
+            if(_time > 5000) {
+                this.scene.start('Game');
+            }
+        }
     }
 
     setupButtons() {
@@ -110,111 +214,5 @@ export class MainMenu extends Scene
             // Lógica para sair do jogo
             console.log('Sair do jogo');
         });
-    }
-
-    create ()
-    {
-        const { width, height } = this.scale;
-
-        this.backgroundCover = this.add.image(this.width / 2, this.height / 2, 'menu-bg').setDepth(-1);
-
-        this.backgroundFar = this.add.tileSprite(0, 0, width, height, 'bg-far');
-        this.backgroundFar.setOrigin(0, 0);
-        this.backgroundFar.setDepth(-20);
-
-        this.backgroundNear = this.add.tileSprite(0, 0, width, height, 'bg-near');
-        this.backgroundNear.setOrigin(0,-0.3);
-        this.backgroundNear.setDepth(-10);
-
-        this.backgroundMiddle = this.add.tileSprite(0, 0, width, height, 'bg-middle');
-        this.backgroundMiddle.setOrigin(0,-0.18);
-        this.backgroundMiddle.setDepth(-15);
-
-        this.backgroundVeryFar = this.add.tileSprite(0, 0, width, height, 'bg-very-far');
-        this.backgroundVeryFar.setOrigin(0, 0);
-        this.backgroundVeryFar.setDepth(-25);
-    }
-
-    update(time: number, delta: number): void{
-        const dt = delta / 1000;
-
-        this.backgroundFar.tilePositionX += MainMenu.PARALLAX_FAR_SPEED * 60 * dt;
-        this.backgroundNear.tilePositionX += MainMenu.PARALLAX_NEAR_SPEED * 60 * dt;
-        this.backgroundMiddle.tilePositionX += MainMenu.PARALLAX_MIDDLE_SPEED * 60 * dt;
-        this.backgroundVeryFar.tilePositionX += MainMenu.PARALLAX_VERY_FAR_SPEED * 60 * dt;
-
-        if(!this.updateChange) {
-            if(time > 2000 && !this.title) {
-                this.title = this.add.image(this.width / 2, 220, 'main-menu-title').setDepth(1);
-            }
-            if(time > 3000 && !this.subtitle) {
-                this.subtitle = this.add.image(this.width / 2, 450, 'main-menu-subtitle').setDepth(2).setScale(0.3);
-            }
-            if(time > 5000 && !this.playButton) {
-                this.playButton = this.add.image(this.width / 2, 800, 'play-button').setScale(this.buttonScale);
-            }
-            if(time > 5200 && !this.configButton) {
-                this.configButton = this.add.image(this.width / 2 - 500, 800, 'config-button').setScale(this.buttonScale);
-            }
-            if(time > 5400 && !this.exitButton) {
-                this.exitButton = this.add.image(this.width / 2 + 500, 800, 'exit-button').setScale(this.buttonScale);
-            }
-            if(time > 6000 && !this.bgTween && !this.fadeOutStarted) {
-                this.fadeOutStarted = true;
-
-                this.bgTween = this.tweens.add({
-                    targets: this.backgroundCover,
-                    alpha: 0,
-                    duration: 500,
-                    ease: 'Linear',
-                });
-                
-                if(this.playButton && this.configButton && this.exitButton && !this.buttonsActive) {
-                    this.setupButtons();
-                }
-            }
-
-        } else {
-            let _time = time - this.timeAtUpdateChange;
-
-            if(this.playButton) {
-                this.playButton.destroy();
-                this.playButton = undefined;
-            }
-
-            this.buttonsActive = false;
-
-            if(_time > 2000 && this.title) {
-                this.title.destroy();
-                this.title = undefined;
-            }
-            if(_time > 1200 && this.subtitle) {
-                this.subtitle.destroy();
-                this.subtitle = undefined;    
-            }
-            if(_time > 200 && this.configButton) {
-                this.configButton.destroy();
-                this.configButton = undefined;
-            }
-            if(_time > 400 && this.exitButton) {
-                this.exitButton.destroy();
-                this.exitButton = undefined;
-            }
-            if(_time > 3000 && this.bgTween && !this.bgTween.isPlaying() && !this.fadeInStarted) {
-                this.fadeInStarted = true;
-
-                this.bgTween?.stop();
-
-                this.bgTween = this.tweens.add({
-                    targets: this.backgroundCover,
-                    alpha: 1,
-                    duration: 500,
-                    ease: 'Linear',
-                });
-            }
-            if(_time > 5000) {
-                this.scene.start('Game');
-            }
-        }
     }
 }
